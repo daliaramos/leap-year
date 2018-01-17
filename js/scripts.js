e40fc128e27b186baa375ea990465e15417297ed
dalia ramos and Katie Fujihara <dalia@>
2018-01-17 15:30:56 -0800

    refactor the requirements for the leap year

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
     $("#result").text(result);
   });
 var leapYear = function(year) {
-    if (year % 4 === 0) {
-      return true;
-  }  else {
-      return false;
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
   }
 };
 });
